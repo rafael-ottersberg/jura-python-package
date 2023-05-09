ded8b2450b83a2c39d14f747b84f9bfe175caba9
Rafael Ottersberg <rafael.ottersberg@gmx.ch>
2023-05-09 12:07:15 +0200

    added long description

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 import setuptools
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
 setuptools.setup(
     name="rv_curve_rafael",
+    long_description=long_description,
     license="GPLv3",
     version="0.0.1",
     author="Rafael",
